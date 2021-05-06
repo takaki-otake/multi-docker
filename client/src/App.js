e600fe21e223fee75e784c95569e1c50708bdd38
takaki.otake <takaki.otake@PC-NO584.local>
2021-05-06 18:19:18 +0900

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hoge Huga Miyo Maru
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
